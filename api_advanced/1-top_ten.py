03619c58bb58ebbee937c7ec15deb55807ac55d7
noel-odero <anoel.odero@gmail.com>
2024-10-25 10:59:30 +0000

    Top Ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
-"""Print the titles of the first 10Hot Posts"""
+"""Print the titles of the first 10 hot posts for a given subreddit"""
 import requests
 
-
 def top_ten(subreddit):
-    """The top ten titles"""
+    """Fetches and prints the titles of the first 10 hot posts for a subreddit."""
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(url, headers=headers)
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
 
-    if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                    json_data.get('data')
-                    .get('children')[i]
-                    .get('data')
-                    .get('title')
-                )
-    else:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        
+        if response.status_code == 200:
+            json_data = response.json()
+            posts = json_data.get('data', {}).get('children', [])
+            
+            # Print titles of up to 10 hot posts
+            for post in posts:
+                print(post.get('data', {}).get('title'))
+            # If there are fewer than 10 posts
+            if len(posts) == 0:
+                print(None)
+                
+        else:
+            print(None)
+            
+    except requests.RequestException:
         print(None)
