a58e38033c073cd94b191b67e181a01e1e588dc7
noel-odero <anoel.odero@gmail.com>
2024-10-25 15:08:21 +0000

    Top Ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,17 +6,23 @@ import requests
 def top_ten(subreddit):
     """The top ten titles"""
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(url, headers=headers)
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
 
-    if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                    json_data.get('data')
-                    .get('children')[i]
-                    .get('data')
-                    .get('title')
-                )
-    else:
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        
+        if response.status_code == 200:
+            json_data = response.json()
+            posts = json_data.get('data', {}).get('children', [])
+            
+            if posts:
+                for post in posts[:10]:
+                    print(post.get('data', {}).get('title'))
+            else:
+                print(None)
+        else:
+            print(None)
+            
+    except requests.RequestException:
         print(None)
